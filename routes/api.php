9107c55b26f44e54db505b607a579c70f34d0210
wenibrando <wenibrando01@example.com>
2026-03-05 23:25:10 +0800

    updated code

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 use App\Http\Controllers\Api\AutController;
 
 Route::get ('/token-test', function () {
-    $user = \app\Models\User::first();
+    $user = \App\Models\User::first();
     return $user->createToken('test')->plainTextToken;
 });
 Route::post('/login', [AutController::class, 'login']);
